cbe2bd8ea5715598259fcbc57eadfa7700e42236
reydon <dhruvarora@delhinerds.com>
2018-12-10 13:44:23 +0530

    registration form changes

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -25,6 +25,10 @@ export class RegisterPage {
     });
   }
 
+  openRegistrationForm(){
+    window.open("http://www.ailbsindiaconference.com/registration");
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
